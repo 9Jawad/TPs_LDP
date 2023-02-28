a31875a75768986e4a74f4deba2de06ed081f59c
Jawad <119442501+9Jawad@users.noreply.github.com>
2023-02-28 20:58:13 +0100

    exo 20

--- séance_3.cpp
+++ séance_3.cpp
@@ -11,7 +11,7 @@ int myMod(int x, int y) {
     return x % y;
 }
 
-int div() {
+void div() {
     int a, b, c;
     cout << "Entrez une valeur pour le dividende 'a':" << endl;
     cin >> a;
@@ -20,15 +20,15 @@ int div() {
     cout << "Entrez une valeur pour le diviseur 'c':" << endl;
     cin >> c;
     cout << "Le résultat est: " << myDiv(a, myMod(b, c)) << endl;
-    return 0;
 }
 
 
-// Exercice 19 ----> pas assimiler encore
+// Exercice 19 ----------------------------------------------------------------
+// variables l, j, k ont un retard d'une incrémentation
 
 //#include <iostream>
 using namespace std;
-int main() {
+void retard() {
     int i = 41; int j = i;
     int k = 666; int l = 0;
     for (i = 40; i < 60; i += 2) {
@@ -45,11 +45,19 @@ int main() {
     }
     cout << "apres la condition if, i vaut : " << i << endl;
     cout << "apres la condition if, j vaut : " << j << endl;
-    return 0;
 }
 
 /// ----- REPONSE :
 /// 5 incrémentations
 /// i vaut : 61, j vaut : 58
 /// k vaut : 56, l vaut : 56
-/// apres if : i and j == 666
+/// apres if : i, j = 666
+
+
+// Exercice 20 ----------------------------------------------------------------
+
+/// en python, nous ne retrouvons pas ce retard là
+
+
+// Exercice 21 ----------------------------------------------------------------
+
